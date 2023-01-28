3f925ba563572041fe5c3bb0768cf2a89b67bb00
J-matsubara <saboteso1867@gmail.com>
2023-01-28 04:32:55 +0000

    PHP05 kadai

---
+++ part3.php
@@ -0,0 +1,10 @@
+<?php
+/*
+$fruits に配列で好きなフルーツを5個代入し、foreach文で順番に出力してください
+*/
+$fruits = array("Apple", "Orange", "Cherry", "Lemon", "Banana");
+
+foreach($fruits as $fruits){
+    echo $fruits;
+    echo "\n";
+}
